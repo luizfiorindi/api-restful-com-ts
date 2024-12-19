87a57b3184595b8a289e4ba6d77d2541211bc391
Luiz Fiorindi <luiz.fiorindi@autlog.com.br>
2024-12-19 15:12:07 -0300

    Implementado o Morgan

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,9 @@ app.use(express.json());
 // Logger
 import Logger from "../config/logger";
 
+// Middlewares
+import morganMiddleware from "./middlewares/morganMiddleware";
+
 //App Port
 const port = config.get<number>("port");
 
@@ -18,6 +21,8 @@ import db from "../config/db";
 
 //APP Routes
 import router from "./router";
+
+app.use(morganMiddleware);
 app.use("/api/", router);
 
 app.listen(3000, async () => {
